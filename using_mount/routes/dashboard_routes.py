b9462fe6baf71b44ace9de3e87b072530f642e47
Pranab Paul <pranabpaul1993@rediffmail.com>
2024-03-10 21:59:08 +0530

    Changes Done.

--- using_mount/routes/dashboard_routes.py
+++ using_mount/routes/dashboard_routes.py
@@ -1,9 +1,10 @@
-from fastapi import FastAPI
+from fastapi import FastAPI , Query
 from models.dashboard_model import *
 from typing import List
 from constants import *
 from routes_utility.dashboard_utility import (
-    get_all_sales_details_data
+    get_all_sales_details_data ,
+    get_region_sales_data
 )
 
 dashboard = FastAPI(docs_url= BASE_PATH.format(ENV= 'dev') + "/docs",
@@ -13,3 +14,8 @@ dashboard = FastAPI(docs_url= BASE_PATH.format(ENV= 'dev') + "/docs",
 @dashboard.get("/all_sales_details", response_model= List[sales_details_model])
 async def get_all_sales_details():
     return await get_all_sales_details_data()
+
+
+@dashboard.post("/region_sales", response_model= List[sales_details_model])
+async def get_region_sales(region:List[str] = Query(['All'])):
+    return await get_region_sales_data(region)
