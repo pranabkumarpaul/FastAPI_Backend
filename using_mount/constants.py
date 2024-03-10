678add4533cf139813d3ba17d8baebaa87ad7b72
Pranab Paul <pranabpaul1993@rediffmail.com>
2024-03-10 21:26:25 +0530

    Changes Done.

--- using_mount/constants.py
+++ using_mount/constants.py
@@ -3,5 +3,15 @@ PORT= 8090
 BASE_PATH= "/api/{ENV}"
 
 GET_EMAIL_DETAILS_QUERY= "select * from public.email;"
+INSERT_EMAIL_DETAILS_QUERY= """
+INSERT INTO public.email
+(id, dept, email_id, subject, body)
+VALUES({ID}, '{DEPT}', '{EMAIL_ID}', '{SUBJECT}', '{BODY}');
+"""
+GET_EMAIL_QUERY= """
+select * from public.email where dept = '{DEPT}' and email_id = '{EMAIL_ID}';
+"""
+
 
 GET_SALES_DETAILS_QUERY= "select * from public.sales;"
+GET_REGION_SALES_QUERY= "select * from public.sales {WHERE_CLAUSE};"
--- using_mount/routes/email_routes.py
+++ using_mount/routes/email_routes.py
@@ -5,6 +5,8 @@ from constants import *
 
 from routes_utility.email_utility import (
     get_all_email_details_data ,
+    get_email_details_data ,
+    insert_email_details_data
 )
 
 email = FastAPI(docs_url= BASE_PATH.format(ENV= 'dev') + "/docs",
@@ -14,3 +16,12 @@ email = FastAPI(docs_url= BASE_PATH.format(ENV= 'dev') + "/docs",
 @email.get("/all_email_details", response_model= List[email_details_model])
 async def get_all_email_details():
     return await get_all_email_details_data()
+
+
+@email.post("/email_details", response_model= List[email_details_model])
+async def get_email_details(user_data:email_query_model):
+    return await get_email_details_data(user_data)
+
+@email.post("/insert_email", response_model= email_details_model)
+async def insert_email_details(user_data: email_details_model):
+    return await insert_email_details_data(user_data)
